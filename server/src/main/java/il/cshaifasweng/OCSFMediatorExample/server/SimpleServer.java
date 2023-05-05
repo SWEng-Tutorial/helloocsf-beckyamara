460ac7ad7d0371e6ed06de371267b45728689aea
beckyamara <beckyamarauni@gmail.com>
2023-05-05 22:33:07 +0300

    updating handleMessageFromClient function

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -6,6 +6,8 @@ import il.cshaifasweng.OCSFMediatorExample.server.ocsf.ConnectionToClient;
 import il.cshaifasweng.OCSFMediatorExample.server.ocsf.SubscribedClient;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class SimpleServer extends AbstractServer {
@@ -13,15 +15,16 @@ public class SimpleServer extends AbstractServer {
 
 	public SimpleServer(int port) {
 		super(port);
-		
+
 	}
 
 	@Override
 	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
 		Message message = (Message) msg;
 		String request = message.getMessage();
+
 		try {
-			//we got an empty message, so we will send back an error message with the error details.
+			//we got an empty message, so we will send back an error message with the error details
 			if (request.isBlank()){
 				message.setMessage("Error! we got an empty message");
 				client.sendToClient(message);
@@ -44,29 +47,89 @@ public class SimpleServer extends AbstractServer {
 			}
 			//we got a message from client requesting to echo Hello, so we will send back to client Hello world!
 			else if(request.startsWith("echo Hello")){
+
 				message.setMessage("Hello World!");
 				client.sendToClient(message);
+
 			}
-			else if(request.startsWith("send Submitters IDs")){
+
+			//added code
+
+			else if (request.startsWith("send Submitters IDs")) {
 				//add code here to send submitters IDs to client
+				String ids = request.substring(20);
+				String fist = ids.substring(0,9);
+				String second = ids.substring(11,19);
+				ids = ""+fist+ ", "+second;
+				message.setMessage(ids);
+				client.sendToClient(message);
 			}
+
 			else if (request.startsWith("send Submitters")){
 				//add code here to send submitters names to client
+				String[] array = request.split(" ", 4);
+
+				String first = array[2];
+				String second = array[3];
+				String names = ""+first+ ", "+second;
+				message.setMessage(names);
+				client.sendToClient(message);
+
+
 			}
 			else if (request.equals("what’s the time?")) {
 				//add code here to send the time to client
+				DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+				LocalTime currentTime = LocalTime.now();
+
+				message.setMessage(currentTime.format(dtf));
+				client.sendToClient(message);
+
+
 			}
 			else if (request.startsWith("multiply")){
 				//add code here to multiply 2 numbers received in the message and send result back to client
 				//(use substring method as shown above)
 				//message format: "multiply n*m"
-			}else{
+				int n=0,m=0,i=9;
+				String result;
+
+				char token = '0';
+				while(token != '*')
+				{
+					n = n * 10;
+					n = n + (token - 48);
+					token = request.charAt(i);
+					i++;
+				}
+
+				do {
+					token = request.charAt(i);
+					m = m * 10;
+					m = m + (token - 48);
+					i++;
+				}while(i < request.length());
+
+
+				result= ""+n+""+"*"+m+""+"= "+(n*m);
+				message.setMessage(result);
+				client.sendToClient(message);
+
+
+
+
+
+			}
+			else{
+
 				//add code here to send received message to all clients.
 				//The string we received in the message is the message we will send back to all clients subscribed.
 				//Example:
-					// message received: "Good morning"
-					// message sent: "Good morning"
+				// message received: "Good morning"
+				// message sent: "Good morning"
 				//see code for changing submitters IDs for help
+
+				sendToAllClients(message);
 			}
 		} catch (IOException e1) {
 			e1.printStackTrace();
